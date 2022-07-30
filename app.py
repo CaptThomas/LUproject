d4fdfdfe2014b8d5639b5f00eb83b81f67365c33
Thomas <19950277+CaptThomas@users.noreply.github.com>
2022-07-30 11:46:52 -0400

    cleanup

--- app.py
+++ app.py
@@ -209,55 +209,55 @@ def register():
 def index():
     """Shows homepage"""
     if request.method == "GET":
-        select_query = "SELECT * FROM feed ORDER BY id DESC;"
+        select_query = "SELECT * FROM feed" + session["user_id"] + " ORDER BY id DESC;"
         row = execute_read_query(connection, select_query)
         return render_template("index.html", rows=len(row), feed=row)
-@app.route("/post", methods=["GET", "POST"])
-@login_required
-def post():
-    """Allows posting of blogs"""
-    if request.method == "POST":
-        if not request.form.get("title"):
-            return render_template("post.html", popup="Create a title for your blog post")
-        elif not request.form.get("content"):
-            return render_template("post.html", popup="Add some content")
-        elif request.form.get("content") == "Start typing...":
-            return render_template("post.html", popup="Add some content")
-        uuidpost = str(uuid.uuid4())
-        datepublished = (date.today()).strftime("%m/%d/%Y")
-        author = session["user_id"]
-        select_query = f"SELECT username FROM users WHERE uuid = '{author}';"
-        rows = execute_read_query(connection, select_query)
-        filename = "templates/blog/" + uuidpost + ".html"
-        f = open(f"{filename}", "a+")
-        f.write("""<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet"><script src="https://code.jquery.com/jquery-3.3.1.min.js"></script><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script><link href="/static/favicon.ico" rel="icon"><title>Orsolingo Community Blogs</title></head><body><div class="container-fluid"><nav class="navbar navbar-inverse"><div class="container-fluid"><div class="navbar-header"><a class="navbar-brand" href="/">Orsolingo</a></div><ul class="nav navbar-nav"> {% if session.user_id %} <li><a href="/">Home</a></li><li><a href="/post">Post Blog</a></li> {% endif %} </ul><ul class="nav navbar-nav navbar-right"> {% if session.user_id %} <li><a href="/logout"> Logout</a></li> {% endif %} </ul></div></nav><main class="container p-5">{% if popup %}<div class="alert alert-danger" role="alert">{{ popup }}</div>{% endif %}""")
-        title = request.form.get("title")
-        title = sanitizer.sanitize(title)
-        f.write(f"""<p class="font-weight-bold" style="text-align:center;">{title}</p>""")
-        author = sanitizer.sanitize(rows[0][0])
-        datepublished = sanitizer.sanitize(datepublished)
-        f.write(f"""<p class="font-weight-light" style="text-align:center;">Written by {author} on {datepublished}</p>""")
-        content = sanitizer.sanitize(request.form.get("content"))
-        f.write(f"""<p class="font-weight-normal" style="text-align:center;"><div class="w-auto p-3" word-wrap: break-word;>{content}</div></p>""")
-        f.write("""<div class="w-100"><div><div class="w-100"></div><h2 style="text-align: center;">Comments</h2></div><form method="post"><div><textarea name="comments" id="comments" style="font-family:sans-serif;font-size:1.2em;">Enter up to 255 characters</textarea></div><input type="submit" value="Submit"></form><div>{% if comments %}{% for x in comments %}<div class="row"><div class="col"><strong>{{x[1]}}</strong></div><div class="col">{{x[2]}}</div><div class="w-100"></div><div class="col" word-wrap: break-word;>{{x[3]}}</div>{% endfor %}{% endif %}</div></div>""")
-        f.write("""</main> </div> </body> </html>""")
-        f.close
-        if len(content) > 100:
-            preview = ""
-            for a in range(100):
-                preview = preview + content[a]
-            preview = preview + "..."
-        else:
-            preview = content
-        query = f"INSERT INTO feed (uuid, title, author, pubdate, contprev) VALUES ('{uuidpost}', '{title}', '{author}', '{datepublished}', '{preview}');"
-        execute_query(connection, query)
-        select_query = f"SELECT id FROM feed WHERE uuid = '{uuidpost}';"
-        rows = execute_read_query(connection, select_query)
-        query = f"CREATE TABLE IF NOT EXISTS comments{rows[0][0]} (id INTEGER PRIMARY KEY, author varchar(255), date varchar(255), body text);"
-        execute_query(connection, query)
-        return redirect(f"/blog/{uuidpost}")
-    else:
-        return render_template("post.html")
+#@app.route("/post", methods=["GET", "POST"])
+#@login_required
+#def post():
+    #"""Allows posting of blogs"""
+    #if request.method == "POST":
+        #if not request.form.get("title"):
+            #return render_template("post.html", popup="Create a title for your blog post")
+        #elif not request.form.get("content"):
+            #return render_template("post.html", popup="Add some content")
+        #elif request.form.get("content") == "Start typing...":
+            #return render_template("post.html", popup="Add some content")
+        #uuidpost = str(uuid.uuid4())
+        #datepublished = (date.today()).strftime("%m/%d/%Y")
+        #author = session["user_id"]
+        #select_query = f"SELECT username FROM users WHERE uuid = '{author}';"
+        #rows = execute_read_query(connection, select_query)
+        #filename = "templates/blog/" + uuidpost + ".html"
+        #f = open(f"{filename}", "a+")
+        #f.write("""<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet"><script src="https://code.jquery.com/jquery-3.3.1.min.js"></script><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script><link href="/static/favicon.ico" rel="icon"><title>Orsolingo Community Blogs</title></head><body><div class="container-fluid"><nav class="navbar navbar-inverse"><div class="container-fluid"><div class="navbar-header"><a class="navbar-brand" href="/">Orsolingo</a></div><ul class="nav navbar-nav"> {% if session.user_id %} <li><a href="/">Home</a></li><li><a href="/post">Post Blog</a></li> {% endif %} </ul><ul class="nav navbar-nav navbar-right"> {% if session.user_id %} <li><a href="/logout"> Logout</a></li> {% endif %} </ul></div></nav><main class="container p-5">{% if popup %}<div class="alert alert-danger" role="alert">{{ popup }}</div>{% endif %}""")
+        #title = request.form.get("title")
+        #title = sanitizer.sanitize(title)
+        #f.write(f"""<p class="font-weight-bold" style="text-align:center;">{title}</p>""")
+        #author = sanitizer.sanitize(rows[0][0])
+        #datepublished = sanitizer.sanitize(datepublished)
+        #f.write(f"""<p class="font-weight-light" style="text-align:center;">Written by {author} on {datepublished}</p>""")
+        #content = sanitizer.sanitize(request.form.get("content"))
+        #f.write(f"""<p class="font-weight-normal" style="text-align:center;"><div class="w-auto p-3" word-wrap: break-word;>{content}</div></p>""")
+        #f.write("""<div class="w-100"><div><div class="w-100"></div><h2 style="text-align: center;">Comments</h2></div><form method="post"><div><textarea name="comments" id="comments" style="font-family:sans-serif;font-size:1.2em;">Enter up to 255 characters</textarea></div><input type="submit" value="Submit"></form><div>{% if comments %}{% for x in comments %}<div class="row"><div class="col"><strong>{{x[1]}}</strong></div><div class="col">{{x[2]}}</div><div class="w-100"></div><div class="col" word-wrap: break-word;>{{x[3]}}</div>{% endfor %}{% endif %}</div></div>""")
+        #f.write("""</main> </div> </body> </html>""")
+        #f.close
+        #if len(content) > 100:
+            #preview = ""
+            #for a in range(100):
+                #preview = preview + content[a]
+            #preview = preview + "..."
+        #else:
+            #preview = content
+        #query = f"INSERT INTO feed (uuid, title, author, pubdate, contprev) VALUES ('{uuidpost}', '{title}', '{author}', '{datepublished}', '{preview}');"
+        #execute_query(connection, query)
+        #select_query = f"SELECT id FROM feed WHERE uuid = '{uuidpost}';"
+        #rows = execute_read_query(connection, select_query)
+        #query = f"CREATE TABLE IF NOT EXISTS comments{rows[0][0]} (id INTEGER PRIMARY KEY, author varchar(255), date varchar(255), body text);"
+        #execute_query(connection, query)
+        #return redirect(f"/blog/{uuidpost}")
+    #else:
+        #return render_template("post.html")
 @app.route("/logout")
 def logout():
     """Logs a user out"""
